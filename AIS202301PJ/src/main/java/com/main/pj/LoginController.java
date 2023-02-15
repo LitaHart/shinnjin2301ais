27553bc0b822fc38d3e65fe72a0a4962236e9129
LitaHart <lita.y.jeon@gmail.com>
2023-02-15 17:34:29 +0900

    Login page

--- AIS202301PJ/src/main/java/com/main/pj/LoginController.java
+++ AIS202301PJ/src/main/java/com/main/pj/LoginController.java
@@ -1,15 +1,22 @@
 package com.main.pj;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private LoginDAO ldao;
+	
+	
 
 	@RequestMapping(value = "/loginPage", method = RequestMethod.GET)
 	public String loginPage() {
-
 		// DB link Check
 		try {
 			postgreSQLconnect.getConnection();
@@ -22,5 +29,16 @@ public class LoginController {
 
 		return "todolist/01_login";
 	}
-
+	
+	
+	@RequestMapping(value = "/mainLogin", method = RequestMethod.POST)
+	public String mainLoginStart(HttpServletRequest Req, Shainn_info si) {
+		
+		ldao.loginStart(Req, si);
+		ldao.loginCheck(Req);
+		
+		return "todolist/01_login";
+	}
+	
+	
 }
---
+++ AIS202301PJ/src/main/java/com/main/pj/Shainn_info.java
@@ -0,0 +1,37 @@
+package com.main.pj;
+
+public class Shainn_info {
+	
+	private String shainn_number;
+	private String 	shainn_name;
+	
+	public Shainn_info() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Shainn_info(String shainn_number, String shainn_name) {
+		super();
+		this.shainn_number = shainn_number;
+		this.shainn_name = shainn_name;
+	}
+
+	public String getShainn_number() {
+		return shainn_number;
+	}
+
+	public void setShainn_number(String shainn_number) {
+		this.shainn_number = shainn_number;
+	}
+
+	public String getShainn_name() {
+		return shainn_name;
+	}
+
+	public void setShainn_name(String shainn_name) {
+		this.shainn_name = shainn_name;
+	}
+	
+	
+	
+
+}
